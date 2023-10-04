124e767ba07caecbcd5bc3669b84d9846af8b970
Cleyson <cleysonph@gmail.com>
2023-10-04 15:16:10 -0300

    feat: add functions to subtract, multiply and divide

--- src/math.spec.js
+++ src/math.spec.js
@@ -5,3 +5,21 @@ describe("math.sum", () => {
     expect(math.sum(1, 2)).toBe(3);
   });
 });
+
+describe("math.subtract", () => {
+  it("should subtract two numbers", () => {
+    expect(math.subtract(1, 2)).toBe(-1);
+  });
+});
+
+describe("math.multiply", () => {
+  it("should multiply two numbers", () => {
+    expect(math.multiply(1, 2)).toBe(2);
+  });
+});
+
+describe("math.divide", () => {
+  it("should divide two numbers", () => {
+    expect(math.divide(1, 2)).toBe(0.5);
+  });
+});
